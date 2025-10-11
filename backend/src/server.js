e7d47a70d12270ae3a803ea29fc2bfaefe117501
123AmaanHussain <ammu.amaanhussain@gmail.com>
2025-10-11 18:19:49 +0530

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+//const express = require('express');
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+dotenv.config({ path: '../.env' });
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+console.log(PORT);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+    console.log('Server is running on port ' + PORT);
+});
