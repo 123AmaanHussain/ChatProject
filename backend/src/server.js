3b1f9c1435d99a2bc97902377988d4367b33920e
123AmaanHussain <ammu.amaanhussain@gmail.com>
2025-12-25 00:20:10 +0530

    Done some minor directory changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,7 +27,7 @@ app.use("/api/messages", messageRoutes);
 if(ENV.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../../frontend/dist')));
     app.get('*', (_, res) => {
-        res.sendFile(path.join(__dirname, "../../frontend","dist","index.html"));
+        res.sendFile(path.join(__dirname, "../../frontend","dist","index.html")); //send index.html for all routes
     });
 }
 
